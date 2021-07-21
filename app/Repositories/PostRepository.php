d1d80d2c381e37920715a78197d3207ef5814df2
G'anisher <git@ganisher.kz>
2021-07-21 23:30:24 +0600

    feat: add Posts (migration, repository, tests)

--- app/Repositories/PostRepository.php
+++ app/Repositories/PostRepository.php
@@ -3,8 +3,24 @@
 
 namespace App\Repositories;
 
+use App\Models\Post;
 
-class PostRepository
+class PostRepository extends BaseRepository
 {
+    /**
+     * @var array
+     */
+    protected $fieldSearchable = [
 
+    ];
+
+    public function model()
+    {
+        return Post::class;
+    }
+
+    public function getFieldsSearchable()
+    {
+        return $this->fieldSearchable;
+    }
 }
